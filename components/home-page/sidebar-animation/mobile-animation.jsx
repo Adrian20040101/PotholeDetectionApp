50b45c361f5485896cfb71c230e613ccc8569e33
Adrian <adrianoinacaru5@gmail.com>
2024-08-01 22:35:22 +0300

    made some adjustments to the sidebar code, splitting it for better encapsulation, and provided a better design for respective device type

---
+++ components/home-page/sidebar-animation/mobile-animation.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, Animated, Pressable, Dimensions } from 'react-native';
+import styles from './mobile-animation.styles';
+
+const MobileSidebar = ({ menuAnim, sidebarVisible, toggleSidebar, menuItems }) => {
+  const { height } = Dimensions.get('window');
+
+  const translateY = menuAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-height, 0],
+  });
+
+  return (
+    <View style={styles.container}>
+      {sidebarVisible && (
+        <Pressable style={styles.overlay} onPress={toggleSidebar} />
+      )}
+      <Animated.View style={[styles.menuContainer, { transform: [{ translateY }] }]}>
+        <View style={styles.menuContent}>
+          {menuItems.map((item, index) => (
+            <Pressable key={index} onPress={item.action} style={styles.menuItem}>
+              <Text style={styles.menuText}>{item.label}</Text>
+            </Pressable>
+          ))}
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+export default MobileSidebar;
