67f471075936fd0704da96813e3de4496ac579bc
Adrian <adrianoinacaru5@gmail.com>
2024-08-15 14:53:53 +0300

    updated the serverless function to handle cors

--- components/home-page/upload-photo/upload-photo.jsx
+++ components/home-page/upload-photo/upload-photo.jsx
@@ -34,7 +34,7 @@ const ImageUpload = () => {
   const triggerServerlessFunction = async (imageUrl) => {
     setAnalyzing(true);
     try {
-      const response = await fetch('../../../netlify/functions/image_analysis.js', {
+      const response = await fetch('https://road-guard.netlify.app/.netlify/functions/image_analysis', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
