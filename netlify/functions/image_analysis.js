fce6537a6439d081c9cc0c8efc5b4ab39d7678b8
Adrian <adrianoinacaru5@gmail.com>
2024-08-15 14:53:53 +0300

    updated the serverless function to handle cors

--- netlify/functions/image_analysis.js
+++ netlify/functions/image_analysis.js
@@ -3,6 +3,20 @@ const fetch = require('node-fetch');
 const pythonScriptPath = "C:/Users/oinac/OneDrive/Desktop/personal_projects/pothole-detection/Pothole-model2/predict-photos.py";
 
 exports.handler = async function (event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: 'This was a preflight call!',
+    };
+  }
+
   const { imageUrl } = JSON.parse(event.body);
 
   console.log(`Image uploaded: ${imageUrl}`);
@@ -11,14 +25,26 @@ exports.handler = async function (event, context) {
     exec(`python3 ${pythonScriptPath} "${imageUrl}"`, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error executing Python script: ${error.message}`);
-        return reject({ statusCode: 500, body: `Error: ${error.message}` });
+        return reject({
+          statusCode: 500,
+          headers,
+          body: JSON.stringify({ error: `Error: ${error.message}` }),
+        });
       }
       if (stderr) {
         console.error(`Python stderr: ${stderr}`);
-        return reject({ statusCode: 500, body: `Error: ${stderr}` });
+        return reject({
+          statusCode: 500,
+          headers,
+          body: JSON.stringify({ error: `Error: ${stderr}` }),
+        });
       }
       console.log(`Python stdout: ${stdout}`);
-      resolve({ statusCode: 200, body: stdout });
+      resolve({
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ result: stdout }),
+      });
     });
   });
 };
